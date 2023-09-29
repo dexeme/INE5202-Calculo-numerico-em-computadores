9342fb47e46be545904223a34dc04d17ee1b4a23
dexeme <dexe.me12@gmail.com>
2023-09-29 17:28:48 -0300

    Diminui o limite

--- Trabalho 2/calcula_multiplicidade.m
+++ Trabalho 2/calcula_multiplicidade.m
@@ -2,7 +2,7 @@ function M = calcula_multiplicidade(R)
     #Avalia a Multiplicidade estimada pelo numero de Restos tendendo a ZERO (menor que um limite (0.1))
     M = 1;
     soma_restos = sum(abs(R(1:2)));
-    while soma_restos < 0.1
+    while soma_restos < 1e-5
         M = M + 1;
         soma_restos = soma_restos + abs(R(M+1));
     end
