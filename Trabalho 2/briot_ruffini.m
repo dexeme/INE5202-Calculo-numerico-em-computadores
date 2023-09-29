7aa225d5be29c4df153319a18791a328c650d8c9
dexeme <dexe.me12@gmail.com>
2023-09-29 14:02:01 -0300

    Atualizando Briot Ruffini

--- Trabalho 2/briot_ruffini.m
+++ Trabalho 2/briot_ruffini.m
@@ -1,12 +1,10 @@
 function [n, a] = briot_ruffini(x, M, n, a)
 # Divisão sintética por Briot Ruffini -> para baixar o grau M vezes
-    for k = 1:M #M divisões 
+    for k = 1:M
         b(1) = a(1);
         for i = 2:n+1
             b(i) = a(i) + x * b(i-1);
-        end
-        
-        # reduzimos o grau, fazendo uma divisão de Briot Ruffini por vez
+                end
         n = n-1;
         a = b(1:n+1);
     end
