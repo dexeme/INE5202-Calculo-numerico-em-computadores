9d59aa85125754cb584e58b15ed7b74137cf247a
dexeme <dexe.me12@gmail.com>
2023-09-29 17:30:03 -0300

    Parando de printar poluição

--- Trabalho 2/mn_raizes_complexas.m
+++ Trabalho 2/mn_raizes_complexas.m
@@ -8,16 +8,19 @@ function [x, M] = mn_raizes_complexas(n, a, xi)
     delta_x = 1;
     iteracao = 0;
     while abs(delta_x) > 1e-14 && iteracao < 100
-        iteracao = iteracao + 1
+        iteracao = iteracao + 1;
         
         # Pn(xi)^M = M! * R(M+1) # R = resto
         # delta_x = -R(M) / (M * R(M+1)) # R = restos da divisão de Pn(x) por x-xi (Briot Ruffini)
-        R = restos(n, a, xi) 
-        M = calcula_multiplicidade(R) # Calcular M em função dos restos (soma de restos < 0.1 indica multiplicidade + 1)
-        delta_x = - R(M) / (M * R(M+1))  #delta_x = - Pn^(M-1) / Pn^(M)
+        R = restos(n, a, xi) ;
+        M = calcula_multiplicidade(R); # Calcular M em função dos restos (soma de restos < 0.1 indica multiplicidade + 1)
+        delta_x = - R(M) / (M * R(M+1));  #delta_x = - Pn^(M-1) / Pn^(M)
         
         # calcular o x final
-        x = xi + delta_x
+        x = xi + delta_x;
         xi = x;
+    end
+    if iteracao == 100
+        disp("Não foi possível encontrar a raiz");
     end
 end
