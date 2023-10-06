02e883693060b702e2c46c86f223d74f0a2e9f48
dexeme <dexe.me12@gmail.com>
2023-10-06 15:11:42 -0300

    Chama função do cálculo para determinar o vetor x

--- Trabalho3/main.m
+++ Trabalho3/main.m
@@ -1,5 +1,12 @@
-x = ;
-c = ;
-xi = ;
+% calculo do intervalo [a : h : b]
+a = 0; b = pi/2 ;
+xi = pi/4; % fixo
+c = [1/2, -1, 0, 2/3]; % Coeficientes -> c(j) = (f' * aux) / j!
+
+numero_de_intervalos = 100;
+h = (b-a)/numero_de_intervalos;
+x = [a : h : b];
 
 Y_aproximado = calcula_valor_polinomio(x, c, xi);
+Y_exato = cos(x).^2;
+erro = max(abs(Y_aproximado - Y_exato))
