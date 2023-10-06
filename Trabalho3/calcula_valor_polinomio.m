8240b07ef30fe717e5eda4de0792d382ca2c6982
dexeme <dexe.me12@gmail.com>
2023-10-06 14:36:50 -0300

    Cálculo do grau e do aux ( x-xi )

--- Trabalho3/calcula_valor_polinomio.m
+++ Trabalho3/calcula_valor_polinomio.m
@@ -1,3 +1,6 @@
-function Y_aproximado = calcula_valor_polinomio(xp, c, xi)
-
+function Y_aproximado = calcula_valor_polinomio(x, c, xi)
+    n = length(c) - 1;
+    for i = 1 : n
+        aux = aux * (x - xi) % Potenciação
+        Y_aproximado = Y_aproximado + c(i + 1) * aux
 end
