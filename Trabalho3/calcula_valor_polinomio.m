ed3ea4e765ffc6ef5cfa3b9379c9510f9b6c858a
dexeme <dexe.me12@gmail.com>
2023-10-06 15:11:52 -0300

    Calcula todos os valores do vetor x e printa

--- Trabalho3/calcula_valor_polinomio.m
+++ Trabalho3/calcula_valor_polinomio.m
@@ -1,6 +1,14 @@
 function Y_aproximado = calcula_valor_polinomio(x, c, xi)
     n = length(c) - 1;
-    for i = 1 : n
-        aux = aux * (x - xi) % Potenciação
-        Y_aproximado = Y_aproximado + c(i + 1) * aux
-end
+    for elem = 1 : length(x)
+        aux = 1; % esse aux é o resultado de (x-xi)
+        elem = 1; % Y_aproximado = [ elem = 1, elem = 2, elem = 3, elem = 4... ]
+        Y_aproximado(elem) = c(1);
+        for i = 1 : n
+            aux = aux * (x(elem) - xi);
+            Y_aproximado(elem) = Y_aproximado + c(i + 1) * aux;
+        end
+    end
+    for k = 1 : length(x)
+        disp(x(k))
+end 
