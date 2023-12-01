63421e3ed8b38dffb823bcf438aed2d79454a5f0
dexeme <dexe.me12@gmail.com>
2023-12-01 15:49:57 -0300

    Coloca import no topo

--- Trabalho-4/m2.m
+++ Trabalho-4/m2.m
@@ -1,6 +1,7 @@
+source("f_gauss_legendre.m");
+
 % 2. Calcule a integral de f(x)=x*ln(x)/(1+cos(x)) entre 0.1 e 1.0, com precisão de 4 dígitos significativos
 %  (pode-se estimar o erro), pelo método mais eficiente (testar Simpson, Gauss-Legendre,...).
-source("f_gauss_legendre.m");
 
 function q2()
     disp("");
