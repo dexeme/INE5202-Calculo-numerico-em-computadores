6936a4b420e4e9ade73265a889e02037193d4b83
dexeme <dexe.me12@gmail.com>
2023-12-01 15:44:15 -0300

    Adiciona questão 1.D

--- Trabalho-4/m1-d.m
+++ Trabalho-4/m1-d.m
@@ -1,3 +1,15 @@
 % 1d. Obtidos os m = 3 pontos/nós t acima, determine os m = 3 
 % coeficientes pesos C do Método de Gauss-Legendre através das 3 primeiras equações, 
 % linearizadas com os 3 t's obtidos no item 1c;
+
+function q1D()
+    disp("");
+    disp("--------------------------------------------");
+    disp("");
+    disp("1.d)");
+    disp("");
+    C = [5/9; 8/9; 5/9];
+    disp("C = [5/9; 8/9; 5/9]");
+    disp("");
+    disp("--------------------------------------------");
+endfunction
