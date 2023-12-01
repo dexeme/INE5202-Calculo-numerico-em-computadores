f9d7fcd8564c2f6eeabba3a41162bf43f38cfc84
dexeme <dexe.me12@gmail.com>
2023-12-01 15:50:08 -0300

    Adiciona arquivo main

---
+++ Trabalho-4/main.m
@@ -0,0 +1,31 @@
+# Importa os arquivos m1-a.m e m1-b.m
+
+source("m1-a.m")
+source("m1-b.m")
+source("m1-c.m")
+source("m1-d.m")
+source("m2.m")
+source("m3.m")
+source("m4.m")
+
+# Carrega questão 1.a)
+
+q1A()
+
+# Carrega questão 1.b)
+
+q1B()
+
+# Carrega questão 1.c)
+
+q1C()
+
+# Carrega questão 1.d)
+
+q1D()
+
+# Carrega questão 2
+
+q2()
+
+
